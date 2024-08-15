acbce987df2a7fa0805b37f46a939e4d4d3b8985
HelenSobi <helensobia88@gmail.com>
2024-08-16 01:42:48 +0400

    change-card-desc

--- src/components/ProjectCardData.jsx
+++ src/components/ProjectCardData.jsx
@@ -9,14 +9,14 @@ const ProjectCardData = [
     {
         imgsrc: simon,
         title: "Simon-Game",
-        text: "A ReactJS game that tests memory by requiring users to repeat increasingly complex sequences of tones and lights.",
+        text: "AA ReactJS-based memory game that sharpens your focus by asking you to repeat sequences of tones and lights that become increasingly difficult.",
         view: "https://simon-game-react.netlify.app/",
         code: "https://github.com/HelenSobi/simon-game"
     },
     {
         imgsrc : Caterar,
         title : "Caterar",
-        text : "Caterar is a platform that allows users to orders for parties and events. Caterar has a community of culinary experts to help make your carving spectacular.",
+        text : "Designed and developed a full-featured culinary catering website with intuitive interfaces, enabling customers to easily navigate and explore the offerings.",
         view : "https://caterar.netlify.app/",
         code :"https://github.com/HelenSobi/caterar"
     },
@@ -24,21 +24,21 @@ const ProjectCardData = [
     {
         imgsrc : Weather,
         title : "Weather Forecast",
-        text : "A simple, beautiful, and responsive weather application created with React.js, Tailwind CSS. It provides current and 7-days weather forecast using visualcrossing API.",
+        text : "A sleek and responsive weather app built with React.js and Tailwind CSS, offering real-time weather updates and a 7-day forecast powered by the Visual Crossing API.",
         view : "https://weatherforecast-7days.netlify.app/",
         code :"https://github.com/HelenSobi/WeatherForecastApp-React"
     },
     {
         imgsrc : Restaurant,
         title : "Food Delivery",
-        text : "Simple , responsive Website for Restaurants, created with React.js, Tailwind CSS, Javascript, HTML. It lists Top Restaurants and its menus using Live API.",
+        text : "A simple, responsive restaurant website built with React.js, Tailwind CSS, featuring a live API to list top restaurants and their menus.",
         view : "https://food-delivery-live-api.netlify.app/",
         code :"https://github.com/HelenSobi/Restaurant-App"
     },
     {
         imgsrc : ToDo,
         title : "To-Do List",
-        text : "A React.js app made to plan our day beforehand and to make it more productive and to utilize ach minute in a positive way. It comes with features like Adding a new task, Deleting the task that is completed, Move the tasks upwards with higher priority and also gave us the chance to edit the tasks as per our needs.",
+        text : "A React.js app designed to enhance daily productivity by allowing users to add, edit, prioritize, and delete tasks, ensuring every minute is used effectively.",
         view : "https://dailynote-planner.netlify.app/",
         code :"https://github.com/HelenSobi/-To_Do_app_ReactJs"
     },
